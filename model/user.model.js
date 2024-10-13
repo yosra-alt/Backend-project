2da7efa238fb3024349530bf06ffcf80fe9e909e
yosra-alt <byosra012@gmail.com>
2024-10-13 10:12:56 -0700

    firstcommit

---
+++ model/user.model.js
@@ -0,0 +1,29 @@
+const schema =mongoose.schema();
+const mongoose = require('mongoose')
+const userSchema = new Schema({
+    FullName: {
+       type:String,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+       },
+   password: {
+        type:String,
+        required:true
+       },
+   role: {
+        type:String,   
+        default:"EMPLOYEE"
+       },
+      
+   profileImage: { type: String },
+
+   status: {
+    type: String, // online, offline
+    default: "offline"
+}
+    
+})
+module.exports.usermodel = mongoose.model('users',userschema)
